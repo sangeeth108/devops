db1d58d5c202afb763d9d177b5abffa09cc91dd3
sangeeth108 <thisaranga108@gmail.com>
2025-02-25 13:31:08 +0530

    test

--- Food Ordering Platform/backend/routes/menuitems.js
+++ Food Ordering Platform/backend/routes/menuitems.js
@@ -4,7 +4,7 @@ const MenuItem = require('../models/MenuItem');
 const router = express.Router();
 
 
-// CREATE a new MenuIte
+// CREATE a new
 router.post('/create/menu-items', async (req, res) => {
     try {
       const { owner, name, description, price, image, quantity } = req.body;
